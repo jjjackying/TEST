776bad40af24643de4cb6938d18baac25a76580f
jjjackying <1823891578@qq.com>
2024-08-08 21:25:49 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("Hello, World!");
+
+public class Test
+{
+    int a = 1;
+    string ab = "第一次上传";
+    void Show()
+    {
+        Console.WriteLine("OK,Now");
+    }
+}
